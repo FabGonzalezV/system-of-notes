be41316ba4e1aee1e5d4592b5d1e8224f329eb23
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2023-03-20 13:52:20 -0600

    added authentication suport

--- models/notes-fs.mjs
+++ models/notes-fs.mjs
@@ -2,7 +2,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import util from 'util';
 import { approotdir } from '../approotdir.mjs';
-import { Note, AbstractNotesStore } from './Notes.mjs';
+import { Note, AbstractNotesStore } from './notes.mjs';
 import { default as DBG } from 'debug';
 const debug = DBG('notes:notes-fs');
 const error = DBG('notes:error-fs');
